605f410404c84845d04ff0d7322070ed89ba242f
Jerson Moreno <jersonmr@gmail.com>
2018-06-12 19:43:00 -0400

    Added auth scaffolding from laravel and credentials for laravel socialite

--- config/services.php
+++ config/services.php
@@ -35,4 +35,16 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    // Socialite
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => '/login/github/callback',
+    ],
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => '/login/facebook/callback',
+    ],
+
 ];
